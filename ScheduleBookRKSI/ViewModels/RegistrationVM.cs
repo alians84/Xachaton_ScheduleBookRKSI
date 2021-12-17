84ab7a20bb78fe4b5e13bca05b77fce708d81b8e
Grigoriy Paravyan <gvalal@bk.ru>
2021-12-18 00:08:34 +0300

    Создаю ViewModel класс окна регистрации

--- ScheduleBookRKSI/ViewModels/RegistrationVM.cs
+++ ScheduleBookRKSI/ViewModels/RegistrationVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScheduleBookRKSI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace ScheduleBookRKSI.ViewModel
 {
-    class RegistrationVM
+    class RegistrationVM : INPC
     {
+
     }
 }
